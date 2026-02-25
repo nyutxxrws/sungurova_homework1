bd838d38b004ca98b46aae76eadeb6a6ac655094
nyutxxrws <nyutxxrws@gmail.com>
2026-02-25 17:29:25 +0300

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,9 +1,10 @@
 user_input = input("Введите число: ")
 if user_input.isdigit() == False:
     print("Ошибка: введено не число")
-elif int(user_input) == 0:
+elif int(user_input) <= 0:
     print("Число должно быть положительным")
 elif int(user_input) % 2 == 0:
     print(f"Число {user_input} является четным")
 else:
+
     print(f"Число {user_input} не является четным")
